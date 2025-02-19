dda06af9568b33ed13d2fbcefce6f2f4a1791614
Talal <talalzafar84@gmail.com>
2025-02-19 16:56:10 +0500

    loyout designed

---
+++ app/src/hooks/useAuth.ts
@@ -0,0 +1,8 @@
+// import {useState} from 'react';
+
+// export const useAuth = () => {
+//   const [user, setUser] = useState(null);
+//   const login = userData => setUser(userData);
+//   const logout = () => setUser(null);
+//   return {user, login, logout};
+// };
---
+++ app/src/navigation/RootNavigator.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import HomeScreen from "../screens/Home/HomeScreen";
+// import LoginScreen from "../screens/Auth/LoginScreen";
+
+// const Stack = createStackNavigator();
+
+// const RootNavigator = () => (
+//   <NavigationContainer>
+//     <Stack.Navigator initialRouteName="Login">
+//       <Stack.Screen name="Login" component={LoginScreen} />
+//       <Stack.Screen name="Home" component={HomeScreen} />
+//     </Stack.Navigator>
+//   </NavigationContainer>
+// );
+
+// export default RootNavigator;
