964d2218c7eb3b3907c3f94c4039577e5de69827
Areeba khan <arkhan.2003k@gmail.com>
2025-05-26 11:09:27 -0700

    ..

--- app/socket.ts
+++
@@ -1,9 +0,0 @@
-// socket.js
-import { io } from 'socket.io-client';
-
-const socket = io('https://backend.gamergizmo.com', {
-  transports: ['websocket'],
-  autoConnect: false,
-});
-
-export default socket;
