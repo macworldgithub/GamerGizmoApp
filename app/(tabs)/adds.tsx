cb79d79779ac98a96b12278f7609364612c95308
Ayesha Fahim <ayesha.fahim02@gmail.com>
2025-06-11 12:16:47 +0500

    ..

--- app/(tabs)/adds.tsx
+++ app/(tabs)/adds.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { View, Text, ScrollView, StyleSheet, TextInput, } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import Toast from "react-native-toast-message";
@@ -9,6 +9,7 @@ import CustomLoader from "./CustomLoader";
 import { router } from "expo-router";
 import { useLocalSearchParams, useFocusEffect } from "expo-router";
 import { useCallback } from "react";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
 
 export default function Adds() {
@@ -16,8 +17,10 @@ export default function Adds() {
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(false);
   const [ads, setAds] = useState([]);
+  const [search, setSearch] = useState("");
+  
 
-  const { refresh } = useLocalSearchParams(); // 👈 get param
+  const { refresh } = useLocalSearchParams();
 
   const fetchAds = async () => {
     try {
@@ -51,16 +54,64 @@ export default function Adds() {
     }
   };
 
- 
+  const handleSearch = async (text: string) => {
+    setSearch(text);
+    try {
+      setLoading(true);
+      const token = await AsyncStorage.getItem("token");
+      
+      if (!token) {
+        router.replace("/login");
+        return;
+      }
+
+      if (!text.trim()) {
+        fetchAds();
+        return;
+      }
+
+      const response = await axios.get(
+        `http://192.168.100.45:4001/products/search-by-name?name=${text}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setAds(response.data.data || []);
+      setTotal(response.data.total || 0);
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Failed to search ads",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
  
 useFocusEffect(
   useCallback(() => {
-    fetchAds();
-  }, [currentPage]) // ✅ safe dependency
+    if (!search.trim()) {
+      fetchAds();
+    }
+  }, [currentPage, search])
 );
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.header}>My Ads</Text>
+     <View className="flex-row items-center bg-gray-100 border border-gray-300 rounded-full px-4 py-2 shadow-sm">
+        <MagnifyingGlassIcon size={20} color="#6b7280" className="mr-2" />
+        <TextInput
+          placeholder="Search for products..."
+          className="flex-1 text-gray-800 text-base"
+          value={search}
+          onChangeText={handleSearch}
+          placeholderTextColor="#9ca3af"
+        />
+      </View>
 
       {loading ? (
         <CustomLoader />
@@ -96,3 +147,5 @@ const styles = StyleSheet.create({
     color: "#000",
   },
 });
+
+
