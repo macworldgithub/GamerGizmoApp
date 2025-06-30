8f90c42ce6708c95c5ebcecdc89d0f99935ca451
ayla-imran851 <ayla@macworldtechnology.com>
2025-06-30 16:08:10 +0500

    ...

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,10 +3,18 @@ import { API_BASE_URL } from "@/utils/config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { useRouter } from "expo-router";
-import {  Pencil } from "lucide-react-native";
-import { Feather } from '@expo/vector-icons';
+import { Pencil } from "lucide-react-native";
+import { Feather } from "@expo/vector-icons";
+import NicUploadModal from "../../components/NicUploadModal";
 import React, { useState } from "react";
-import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useDispatch, useSelector } from "react-redux";
 import CitySelectorModal from "../(tabs)/CityModal";
@@ -14,13 +22,19 @@ import ContactModal from "../(tabs)/ContactModal";
 import LanguageModal from "../(tabs)/LanguageModal";
 import TermsModal from "../(tabs)/TermsModal";
 import { LogoutUser } from "../../store/slice/loginSlice";
-import EditProfilePhotoModal from './EditProfilePhotoModal';
-import { Entypo, FontAwesome5, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import EditProfilePhotoModal from "./EditProfilePhotoModal";
+import {
+  Entypo,
+  FontAwesome5,
+  Ionicons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import GetStartedBadge from "@/components/GetStartedBadge";
 
 const Profile = () => {
   const router = useRouter();
   const user = useSelector((state: RootState) => state.user);
-  const [modalVisible, setModalVisible] = useState(false); 
+  const [modalVisible, setModalVisible] = useState(false);
   const [profileImage, setProfileImage] = useState(
     user?.profile ? { uri: user.profile } : null
   );
@@ -30,8 +44,8 @@ const Profile = () => {
   const [languageModalVisible, setLanguageModalVisible] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState("English");
   const [termsModalVisible, setTermsModalVisible] = useState(false);
+  const [openNicModal, setOpenNicModal] = useState(false);
   const dispatch = useDispatch();
-
 
   React.useEffect(() => {
     if (user?.profile) {
@@ -45,7 +59,6 @@ const Profile = () => {
         <View className="flex-row items-center space-x-4 gap-4">
           <View className="items-center ">
             <TouchableOpacity onPress={() => setModalVisible(true)}>
-
               <View className="relative">
                 {profileImage ? (
                   <>
@@ -53,26 +66,26 @@ const Profile = () => {
                       source={{ uri: profileImage.uri }}
                       style={{ width: 70, height: 70, borderRadius: 50 }}
                     />
-                      </>
-                    ) : (
-                    <View
-                      style={{
-                        width: 70,
-                        height: 70,
-                        borderRadius: 50,
-                        backgroundColor: '#f0f0f0',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <FontAwesome name="user-circle" size={70} color="#999" />
-                    </View>
+                  </>
+                ) : (
+                  <View
+                    style={{
+                      width: 70,
+                      height: 70,
+                      borderRadius: 50,
+                      backgroundColor: "#f0f0f0",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <FontAwesome name="user-circle" size={70} color="#999" />
+                  </View>
                 )}
 
-                    <View className="absolute bottom-0 right-0 bg-white rounded-full p-1 shadow">
-                      <Pencil size={16} color="#000" />
-                    </View>
-                  </View>
+                <View className="absolute bottom-0 right-0 bg-white rounded-full p-1 shadow">
+                  <Pencil size={16} color="#000" />
+                </View>
+              </View>
             </TouchableOpacity>
 
             <EditProfilePhotoModal
@@ -88,42 +101,40 @@ const Profile = () => {
                 ? `${user.first_name} ${user.last_name}`
                 : "Not Logged In"}
             </Text>
-            <View className="flex-row items-center space-x-2 mt-1 border border-gray-300 ">
-              <Text className="text-xs text-gray-600   px-2 py-1 ml-2">
-                Get Verified
-              </Text>
-              <FontAwesome name="check-circle" size={14} color="purple" />
-            </View>
+            <TouchableOpacity
+              onPress={() => setOpenNicModal(true)}
+              className=" px-4 py-2 rounded-lg"
+            >
+              <View className="border border-gray-200 px-4 py-2 rounded-lg flex-row gap-2 ">
+                <Text className="text-black text-xs">Get Verified</Text>
+
+                <FontAwesome name="check-circle" size={14} color="purple" />
+              </View>
+            </TouchableOpacity>
+            <NicUploadModal open={openNicModal} setOpen={setOpenNicModal} />
             <Text className="text-xs text-gray-500 mt-3">
               Joined on {user?.created_at?.slice(0, 10) || "Unknown"}
             </Text>
           </View>
         </View>
-
       </View>
 
       <View className="flex-row justify-center space-x-4 mt-6 mx-4">
-        <TouchableOpacity className="border border-gray-200 px-6 py-3 rounded-xl w-40 flex-column items-center justify-center mr-7 ">
+        <TouchableOpacity
+          onPress={() => router.push("/adds")}
+          className="border border-gray-200 px-6 py-3 rounded-xl w-40 flex-column items-center justify-center mr-7 "
+        >
           <FontAwesome name="th-list" size={18} color="purple" />
-          <Text
-            className="text-black font-bold ml-2 mt-3"
-            onPress={() => router.push("/adds")}
-          >
-            My Ads
-          </Text>
+          <Text className="text-black font-bold ml-2 mt-3">My Ads</Text>
         </TouchableOpacity>
-        <TouchableOpacity className="border border-gray-200 px-6 py-3 rounded-xl w-40 flex-column items-center justify-center mr-7 "
-        onPress={() => router.push("/store")}
+        <TouchableOpacity
+          className="border border-gray-200 px-6 py-3 rounded-xl w-40 flex-column items-center justify-center mr-7 "
+          onPress={() => router.push("/store")}
         >
           <FontAwesome5 name="store" size={18} color="purple" />
           {/* <FontAwesome name="fa-solid fa-store" size={18} color="purple" /> */}
-          <Text
-            className="text-black font-bold ml-2 mt-3"
-          >
-            Store
-          </Text>
+          <Text className="text-black font-bold ml-2 mt-3">Store</Text>
         </TouchableOpacity>
-
       </View>
 
       <View className="mt-6 px-6 gap-3">
@@ -138,11 +149,12 @@ const Profile = () => {
           <Image
             source={require("../../assets/images/next.png")}
             className="w-4 h-4"
-          />  
+          />
         </TouchableOpacity>
 
-        <TouchableOpacity className="flex-row items-center justify-between py-4"
-        onPress={() => router.push("/AddToCart")}
+        <TouchableOpacity
+          className="flex-row items-center justify-between py-4"
+          onPress={() => router.push("/AddToCart")}
         >
           <View className="flex-row items-center space-x-3">
             <FontAwesome5 name="cart-plus" size={22} color="#9341f3" />
@@ -154,8 +166,9 @@ const Profile = () => {
           />
         </TouchableOpacity>
 
-        <TouchableOpacity className="flex-row items-center justify-between py-4 border-b border-gray-200"
-        onPress={() => router.push("/Orders")}
+        <TouchableOpacity
+          className="flex-row items-center justify-between py-4 border-b border-gray-200"
+          onPress={() => router.push("/Orders")}
         >
           <View className="flex-row items-center space-x-3">
             <FontAwesome5 name="shopping-bag" size={22} color="#9341f3" />
@@ -207,8 +220,10 @@ const Profile = () => {
           onSelect={(lang: any) => setSelectedLanguage(lang)}
         />
 
-        <TouchableOpacity className="flex-row items-center justify-between py-4 "
-          onPress={() => router.push("/blog")}>
+        <TouchableOpacity
+          className="flex-row items-center justify-between py-4 "
+          onPress={() => router.push("/blog")}
+        >
           <View className="flex-row items-center space-x-3">
             <MaterialIcons name="article" size={22} color="#9341f3" />
 
@@ -282,23 +297,27 @@ const Profile = () => {
               );
 
               if (response.status === 200 || response.status === 201) {
-
                 Alert.alert("Success", "You have been logged out.");
                 await AsyncStorage.removeItem("token");
                 await AsyncStorage.removeItem("user");
                 dispatch(LogoutUser());
                 router.replace("/login");
               } else {
-                console.warn("Logout non-200 response:", response.status, response.data);
+                console.warn(
+                  "Logout non-200 response:",
+                  response.status,
+                  response.data
+                );
                 Alert.alert("Logout Failed", "Unexpected server response.");
               }
             } catch (error: any) {
-              console.error("Logout error", error.response?.data || error.message);
+              console.error(
+                "Logout error",
+                error.response?.data || error.message
+              );
               Alert.alert("Error", "Failed to log out. Please try again.");
             }
           }}
-
-
         >
           <View className="flex-row items-center space-x-3">
             <Image
@@ -312,7 +331,6 @@ const Profile = () => {
             className="w-4 h-4"
           />
         </TouchableOpacity>
-
       </View>
     </ScrollView>
   );
